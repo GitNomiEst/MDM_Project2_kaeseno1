b32d9b6642e620d86b87d2787b5e88dc8c551819
Noémie <kaeseno1@students.zhaw.ch>
2024-04-16 09:28:59 +0200

    Setup Java Project

---
+++ src/main/java/ch/zhaw/nk/mdm_project2_kaeseno1/MdmProject2Kaeseno1Application.java
@@ -0,0 +1,13 @@
+package ch.zhaw.nk.mdm_project2_kaeseno1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmProject2Kaeseno1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmProject2Kaeseno1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/nk/mdm_project2_kaeseno1/MdmProject2Kaeseno1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.nk.mdm_project2_kaeseno1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmProject2Kaeseno1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

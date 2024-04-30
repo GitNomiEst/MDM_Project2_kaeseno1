12653d7f73e08d4422d54003c093d76e6f1bd552
Noémie <kaeseno1@students.zhaw.ch>
2024-04-30 12:46:37 +0200

    unstable version

--- src/main/java/ch/zhaw/nk/mdm_project2_kaeseno1/ServiceController.java
+++ src/main/java/ch/zhaw/nk/mdm_project2_kaeseno1/ServiceController.java
@@ -3,23 +3,39 @@ package ch.zhaw.nk.mdm_project2_kaeseno1;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import ch.zhaw.nk.mdm_project2_kaeseno1.ml.SurvivalInference;
 import ch.zhaw.nk.mdm_project2_kaeseno1.model.Guest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class ServiceController {
 
     DataAccessor dataAccessor = new DataAccessor();
+    SurvivalInference survivalInference = new SurvivalInference(); // Instantiate SurvivalInference
 
     @GetMapping("/guests")
     public ArrayList<Guest> getIndex(){
         ArrayList<Guest> guests = new ArrayList<>();
         guests = dataAccessor.getAllEntries();
         return guests;
-
     }
 
-    
-    
+    @PostMapping("/predict")
+    public String predictSurvival(
+            @RequestParam("sex") String sex,
+            @RequestParam("age") int age,
+            @RequestParam("class") String passengerClass
+    ) {
+        // Call a method to predict survival based on the input data
+        String survivalResult = survivalInference.predictSurvival(sex, age, passengerClass);
+
+        // Return the prediction result as a String
+        if (survivalResult == "Survived") {
+            return "You would survive!";
+        } else {
+            return "Unfortunately, you would not survive.";
+        }
+    }
 }
